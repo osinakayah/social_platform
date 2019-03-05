a97ca4bcbf2103f635b4375242797c66550c2d43
Ifeanyi Osinakayah Joseph <ifeanyiosinakayah15@gmail.com>
2019-03-05 10:39:04 +0100

    Added gravatar

--- app/helpers/comments_helper.rb
+++ app/helpers/comments_helper.rb
@@ -1,2 +1,7 @@
 module CommentsHelper
+  def poster_avatar(email)
+    hash = Digest::MD5.hexdigest(email)
+    image_src = "https://www.gravatar.com/avatar/#{hash}"
+    image_tag image_src, class: "img-fluid img-circle rounded-circle"
+  end
 end
--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,4 +1,5 @@
 class Comment < ApplicationRecord
   validates_presence_of :comment, :post_id
   belongs_to :post
+  belongs_to :user
 end
